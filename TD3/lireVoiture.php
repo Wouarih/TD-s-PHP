96c391ea3b07d99b4b4912743087e9cde803e1c3
Clément Oiry <clement.oiry@etu.umontpellier.fr>
2022-02-23 11:55:13 +0100

    Début TD3

---
+++ TD3/lireVoiture.php
@@ -0,0 +1,28 @@
+<?php
+
+//-> appel fonction / :: appel fonction static
+
+require_once 'Model.php';
+require_once 'Voiture.php';
+
+$rep = Model::getPDO()->query("SELECT * FROM voiture");
+
+
+/*$tab_obj = $rep->fetchAll(PDO::FETCH_OBJ);*/
+
+$rep->setFetchMode(PDO::FETCH_CLASS, 'Voiture');
+
+$tab_voit = $rep->fetchAll();
+
+foreach($tab_voit as $key){
+        $key->afficher();
+}
+
+$car = new Voiture("CASOUALE", "Citroen", "Noir");
+
+//Voiture::getVoitureByImmat("0A100E2S");
+
+//$car->save();
+
+
+
