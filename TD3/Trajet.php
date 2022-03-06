1a3e4546d9754545509aa3d9cc58fc2de468a38b
Clément Oiry <clement.oiry@etu.umontpellier.fr>
2022-03-06 15:15:57 +0100

    modification classes utilisateur et trajet

--- TD3/Trajet.php
+++ TD3/Trajet.php
@@ -1,55 +1,63 @@
 <?php
 
-class Trajet{
+require_once 'Model.php';
+require_once 'Utilisateur.php';
+
+class Trajet {
 
     private $id;
     private $depart;
-    private $arrive;
+    private $arrivee;
     private $date;
-    private $nbplace;
+    private $nbplaces;
     private $prix;
     private $conducteur_login;
 
-    /**
-     * @param $id
-     * @param $depart
-     * @param $arrive
-     * @param $date
-     * @param $nbplace
-     * @param $prix
-     * @param $conducteur_login
-     */
-
-
-    public function get($nom_attribut){
+    // Getter générique (pas expliqué en TD)
+    public function get($nom_attribut) {
         if (property_exists($this, $nom_attribut))
             return $this->$nom_attribut;
         return false;
     }
 
-    public function set($nom_attribut, $valeur){
+    // Setter générique (pas expliqué en TD)
+    public function set($nom_attribut, $valeur) {
         if (property_exists($this, $nom_attribut))
-            $this->nom_attribut = $valeur;
+            $this->$nom_attribut = $valeur;
         return false;
     }
 
-    public function __construct(){
-        if (!empty($data)){
-            $this->id=$data["id"];
-            $this->depart=$data["depart"];
-            $this->arrive=$data["arrive"];
-            $this->date=$data["date"];
-            $this->nbplaces=$data["nbplaces"];
-            $this->prix=$data["prix"];
-            $this->conducteur_login=$data["conducteur-login"];
+    // un constructeur
+    public function __construct($data = array()) {
+        if (!empty($data)) {
+            $this->id = $data["id"];
+            $this->depart = $data["depart"];
+            $this->arrivee = $data["arrivee"];
+            $this->date = $data["date"];
+            $this->nbplaces = $data["nbplaces"];
+            $this->prix = $data["prix"];
+            $this->conducteur_login = $data["conducteur_login"];
+        }
     }
-}
+    // une methode d'affichage.
+    public function afficher() {
+        echo "Ce trajet du {$this->date} partira de {$this->depart} pour aller à {$this->arrivee}.";
+    }
 
-public function afficher(){
-        echo "<p> Ce trajet du {$this->date} partira de {$this->depart} pour aller à {$this->arrive}.";
-}
-
-
-
-
+    public static function getAllTrajets() {
+        try {
+            $pdo = Model::$pdo;
+            $sql = "SELECT * from trajet";
+            $rep = $pdo->query($sql);
+            $rep->setFetchMode(PDO::FETCH_CLASS, 'Trajet');
+            return $rep->fetchAll();
+        } catch (PDOException $e) {
+            if (Conf::getDebug()) {
+                echo $e->getMessage(); // affiche un message d'erreur
+            } else {
+                echo 'Une erreur est survenue <a href=""> retour a la page d\'accueil </a>';
+            }
+            die();
+        }
+    }
 }
--- TD3/Utilisateur.php
+++ TD3/Utilisateur.php
@@ -1,79 +1,56 @@
 <?php
 
-class Utilisateur{
+require_once 'Model.php';
+require_once 'Trajet.php';
+
+class Utilisateur {
 
     private $login;
     private $nom;
     private $prenom;
 
-    /**
-     * @param $login
-     * @param $nom
-     * @param $prenom
-     */
-
-    public function __construct($login, $nom, $prenom)
-    {
-        $this->login = $login;
-        $this->nom = $nom;
-        $this->prenom = $prenom;
+    // Getter générique (pas expliqué en TD)
+    public function get($nom_attribut) {
+        if (property_exists($this, $nom_attribut))
+            return $this->$nom_attribut;
+        return false;
     }
 
-    /**
-     * @return mixed
-     */
-    public function getLogin()
-    {
-        return $this->login;
+    // Setter générique (pas expliqué en TD)
+    public function set($nom_attribut, $valeur) {
+        if (property_exists($this, $nom_attribut))
+            $this->$nom_attribut = $valeur;
+        return false;
     }
 
-    /**
-     * @param mixed $login
-     */
-    public function setLogin($login)
-    {
-        $this->login = $login;
+    // un constructeur
+    public function __construct($login = NULL, $nom = NULL, $prenom = NULL) {
+        if (!is_null($login) && !is_null($nom) && !is_null($prenom)) {
+            $this->login = $login;
+            $this->nom = $nom;
+            $this->prenom = $prenom;
+        }
     }
 
-    /**
-     * @return mixed
-     */
-    public function getNom()
-    {
-        return $this->nom;
+    // une methode d'affichage.
+    public function afficher() {
+        echo "Utilisateur {$this->prenom} {$this->nom} de login {$this->login}";
     }
 
-    /**
-     * @param mixed $nom
-     */
-    public function setNom($nom)
-    {
-        $this->nom = $nom;
+    public static function getAllUtilisateurs() {
+        try {
+            $pdo = Model::$pdo;
+            $sql = "SELECT * from utilisateur";
+            $rep = $pdo->query($sql);
+            $rep->setFetchMode(PDO::FETCH_CLASS, 'Utilisateur');
+            return $rep->fetchAll();
+        } catch (PDOException $e) {
+            if (Conf::getDebug()) {
+                echo $e->getMessage(); // affiche un message d'erreur
+            } else {
+                echo 'Une erreur est survenue <a href=""> retour a la page d\'accueil </a>';
+            }
+            die();
+        }
     }
-
-    /**
-     * @return mixed
-     */
-    public function getPrenom()
-    {
-        return $this->prenom;
-    }
-
-    /**
-     * @param mixed $prenom
-     */
-    public function setPrenom($prenom)
-    {
-        $this->prenom = $prenom;
-    }
-
-    public function afficher(){
-        echo "<p> Utilisateur {$this->prenom} {$this->nom} de login {$this->login}</p>";
-    }
-
-
-
 }
-
-
-?>
